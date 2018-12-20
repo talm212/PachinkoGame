cba5e1c701e33e7e5497f7999f1cef78e9722336
Tal Mishali <talm@playstudios-il.com>
2018-12-20 16:23:06 +0200

    return app

---
+++ src/main/java/FinalProject/Pachinko/Hello.java
@@ -0,0 +1,14 @@
+package FinalProject.Pachinko;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+public class Hello {
+
+    @RequestMapping("/")
+    public String index() {
+        return "Lior is the king!";
+    }
+
+}
